d3cbe70ef599cbebde2ba9c9e00b2a75875f5156
fanagiannis <foulis67@gmail.com>
2024-04-22 19:43:42 +0300

    first commit

---
+++ Game.py
@@ -0,0 +1,22 @@
+import pygame as pg
+
+from Settings import*
+
+pg.init()
+
+WIN=pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pg.display.set_caption("DOOM CLONE")
+CLOCK=pg.time.Clock()
+
+
+
+while True:
+    for event in pg.event.get():
+        if event.type==pg.QUIT :
+            pg.quit()
+
+    pg.display.flip()
+    CLOCK.tick(FPS)
+        
+        
+                        
---
+++ Player.py
@@ -0,0 +1,12 @@
+import pygame as pg 
+
+class PLayer:
+    def __init__(self):
+        self.speed=2
+    
+    def draw(self):
+        pass
+    def move(self):
+        pass
+    def update(self):
+        pass
