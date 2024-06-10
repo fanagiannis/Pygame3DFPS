ac2601b3cd6bff533f44399bd141e2924a78d8b3
fanagiannis <foulis67@gmail.com>
2024-06-10 17:46:12 +0300

    low fps

--- Floorcaster.py
+++ Floorcaster.py
@@ -5,7 +5,7 @@ from Settings import *
 
 class Floorcaster():
     def __init__(self):
-        self.HRES=100
+        self.HRES=120
         self.VERTICAL=100
         self.HALFRES=int(self.VERTICAL/2)
         self.MOD=self.HALFRES/60
@@ -28,24 +28,26 @@ class Floorcaster():
         DISPLAY.blit(surf,(0,0))
 
     def LoadFrames(self,player):
-        for i in range(self.HRES):
-            ROT_i=self.ROT+np.deg2rad(i/self.MOD-30)
-            sin,cos,cos2=np.sin(ROT_i),np.cos(ROT_i),np.cos(np.deg2rad(i/self.MOD-30))
-
-            self.FRAME[i][:]=self.sky[int(np.rad2deg(ROT_i)%359)][:]/255    
-
-            for j in range (self.HALFRES):
-                n=(self.HALFRES / (self.HALFRES- j))/cos2
-                #x,y= self.POSX*0.1+cos*n,self.POSY*0.1 +sin*n 
-                x,y= self.POSY*0.1+cos*n,-self.POSX*0.1 +sin*n                                          #!!!WARNING! REVERSE X AND Y FROM PLAYER MOVEMENT! y=-PLAYER POSISTION ON Y!
-
-                gx,gy=int(x*1%1*100),int(y*1%1*100)                                                     #gx,gy = ground x and ground y  HERE YOU CAN ADJUST THE RESOLUTION SCALE
-                shader=0.2+0.8*(1-j/self.HALFRES)                                              
-                self.FRAME[i][self.HALFRES*2-j-1]=shader*self.floor[gx][gy]/255
-
         self.POSX,self.POSY=player.get_pos()
         self.ROT=player.get_angle()
+        frame=self.FrameCalculation(self.HRES,self.MOD,self.HALFRES,self.POSX,self.POSY,self.ROT,self.floor,self.FRAME)
         
+    def FrameCalculation(self,HRES,MOD,HALFRES,POSX,POSY,ROT,floor,frame):
+        for i in range(HRES):
+            ROT_i=ROT+np.deg2rad(i/MOD-30)
+            sin,cos,cos2=np.sin(ROT_i),np.cos(ROT_i),np.cos(np.deg2rad(i/MOD-30))
+
+            frame[i][:]=self.sky[int(np.rad2deg(ROT_i)%359)][:]/255    
+
+            for j in range (HALFRES):
+                n=(HALFRES / (HALFRES- j))/cos2
+                #x,y= self.POSX*0.1+cos*n,self.POSY*0.1 +sin*n 
+                x,y= POSY*0.1+cos*n,-POSX*0.1 +sin*n                                          #!!!WARNING! REVERSE X AND Y FROM PLAYER MOVEMENT! y=-PLAYER POSISTION ON Y!
+
+                gx,gy=int(x*1%1*100),int(y*1%1*100)                                                     #gx,gy = ground x and ground y  HERE YOU CAN ADJUST THE RESOLUTION SCALE
+                shader=0.2+0.8*(1-j/HALFRES)                                              
+                frame[i][HALFRES*2-j-1]=shader*floor[gx][gy]/255
+        return frame
 
     def Update(self,player): 
         self.LoadFrames(player)
