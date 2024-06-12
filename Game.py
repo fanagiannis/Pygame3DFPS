ee96adeb196f0fbdcb4604fed895f5a7879cf822
fanagiannis <foulis67@gmail.com>
2024-06-12 11:12:45 +0300

    nothing works

--- Game.py
+++ Game.py
@@ -9,7 +9,8 @@ from TextureLoader import *
 
 class Game:
     def __init__(self):
-        pg.mouse.set_visible(False)
+        
+        self.DISPLAY=pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
         self.running=True
         self.clock=pg.time.Clock()
         self.DELTA_TIME=self.clock.tick(FPS)
@@ -17,40 +18,39 @@ class Game:
         #self.LoadMaps()
         self.player=Player(self,self.map)
         self.RayCaster=Raycaster(self,80,self.player,self.map)
-        self.FloorCaster=Floorcaster()
+        self.FloorCaster=Floorcaster(self)
         self.Textureloader=TextureLoader()
         
-        pass
-
     def Run(self):
         pg.init()
         while self.running:
             self.Events()
-            pg.display.set_caption(f'{self.clock.get_fps():.1f}')
-            #pg.display.flip()
-            DISPLAY.fill((0,0,0))
-            self.DELTA_TIME=self.clock.tick(FPS)
+            self.draw()
             self.Update()
-            pg.display.update()
+
+    def draw(self):
+        self.DISPLAY.fill('black')
+        self.player.draw()
+        self.map.draw()
 
     def Events(self):
         for event in pg.event.get():
             if event.type==pg.QUIT :
                 pg.quit()    
     
-    def LoadMaps(self):
-        self.map=Map(MAP_SMALL)
-        #self.map_m=Map(MAP_MED)
-        self.selectedmap=self.map
     def GetMap(self):
         return self.map
 
     def Update(self):
+        pg.display.flip()
+        pg.display.set_caption(f'{self.clock.get_fps():.1f}')  
+        self.DELTA_TIME=self.clock.tick(FPS)
         #self.FloorCaster.Update(self.player)
         #self.RayCaster.update()
-        self.map.update()
+        #self.map.update()
         self.player.update()
         #self.Textureloader.update()
+        pass
         
 
 if __name__=='__main__':
--- Player.py
+++ Player.py
@@ -7,50 +7,62 @@ from Map import*
 class Player:
     def __init__(self,game,map):
         self.game=game
-        self.speed=0.1
-        self.pos=((int(SCREEN_WIDTH/2)/2),(int(SCREEN_WIDTH/2)/2))
+        self.speed=0.1*self.game.DELTA_TIME
+        self.pos=(10,60)
         self.posx,self.posy=self.pos
         
-        self.angle=math.pi
+        self.angle=0
         self.FOV=math.pi/3
         self.HFOV=self.FOV/2
         self.forward=False
 
         self.curmap=map
-        print(self.game.map.worldmap)
 
         
     
     def draw(self):
-        self.look()  
-        pg.draw.circle(DISPLAY,(255,0,0),(int(self.posx),int(self.posy)),8)
+        
+        pg.draw.circle(self.game.DISPLAY,(255,0,0),(int(self.posx),int(self.posy)),1)
+        pg.draw.line(self.game.DISPLAY,'yellow',(self.posx*50,self.posy*50),(self.posx*50+SCREEN_WIDTH,self.posy*50+SCREEN_WIDTH),2)
         pass
-    
-    def look(self):
-        #pg.draw.line(DISPLAY,(0,255,0),(self.get_pos()),(int(self.posx-math.sin(self.angle-self.HFOV)*50),int(self.posy+math.cos(self.angle-self.HFOV)*50)),1)
-        #pg.draw.line(DISPLAY,(0,255,0),(self.get_pos()),(int(self.posx-math.sin(self.angle+self.HFOV)*50),int(self.posy+math.cos(self.angle+self.HFOV)*50)),1)
-        pg.draw.line(DISPLAY,(0,255,0),(self.get_pos()),(int(self.posx-math.sin(self.angle)*50),int(self.posy+math.cos(self.angle)*50)),1)
 
     def move(self):
         keys=pg.key.get_pressed()
         #self.map_collision(self.posx,self.posy)
         
+        sin_a=math.sin(self.angle)
+        cos_a=math.cos(self.angle)
 
         dx,dy=0,0
-        
+        self.speed=0.1*self.game.DELTA_TIME
         if keys[pg.K_w]:
-            self.forward=True
-            dx+=-math.sin(self.angle)*self.speed*self.game.DELTA_TIME
-            dy+=math.cos(self.angle)*self.speed*self.game.DELTA_TIME
+            dx+=self.speed*cos_a
+            dy+=self.speed*sin_a
         if keys[pg.K_s]:
-            self.forward=False
-            dx-=-math.sin(self.angle)*self.speed*self.game.DELTA_TIME
-            dy-=math.cos(self.angle)*self.speed*self.game.DELTA_TIME
-        #print(self.check_wall(self.posx,self.posy))
-        self.map_collision(dx,dy)
+            dx+=-self.speed*cos_a
+            dy+=-self.speed*sin_a
+        if keys[pg.K_a]:
+            dx+=self.speed*sin_a
+            dy+=-self.speed*cos_a
+        if keys[pg.K_d]:
+            dx+=-self.speed*sin_a
+            dy+=self.speed*cos_a
 
-        if keys[pg.K_a]:self.angle-=0.01*self.game.DELTA_TIME
-        if keys[pg.K_d]:self.angle+=0.01*self.game.DELTA_TIME
+        self.posx+=dx
+        self.posy+=dy
+        # if keys[pg.K_w]:
+        #     self.forward=True
+        #     dx+=-math.sin(self.angle)*self.speed*self.game.DELTA_TIME
+        #     dy+=math.cos(self.angle)*self.speed*self.game.DELTA_TIME
+        # if keys[pg.K_s]:
+        #     self.forward=False
+        #     dx-=-math.sin(self.angle)*self.speed*self.game.DELTA_TIME
+        #     dy-=math.cos(self.angle)*self.speed*self.game.DELTA_TIME
+        #print(self.check_wall(self.posx,self.posy))
+        #self.map_collision(dx,dy)
+
+        if keys[pg.K_LEFT]:self.angle-=0.01*self.game.DELTA_TIME
+        if keys[pg.K_RIGHT]:self.angle+=0.01*self.game.DELTA_TIME
         self.angle%=math.tau
 
     def check_wall(self,x,y):
@@ -71,22 +83,21 @@ class Player:
         #     else:
         #         self.posx+=-math.sin(self.angle)*self.speed*DELTA_TIME
         #         self.posy+=math.cos(self.angle)*self.speed*DELTA_TIME
-        pass
 
+    @property
     def get_pos(self):
         return self.posx,self.posy
-
+    @property
     def get_angle(self):
         return self.angle
-    
+    @property
     def get_map_pos(self):
         return int(self.posx),int(self.posy)
     
     def update(self):
         #self.map_collision()
         self.move()
-        #self.mousecontrol()
-        self.draw()
+        #self.mousecontrol()    
         pass
 
 if __name__=='__main__':
