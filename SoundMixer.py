91e6db321dee903753552312f8a1d555f8e2e8db
fanagiannis <foulis67@gmail.com>
2024-06-19 12:52:01 +0300

    IMPORTED DAGGERFALL SFX

--- SoundMixer.py
+++ SoundMixer.py
@@ -4,14 +4,21 @@ import random
 class SoundMixer():
     def __init__(self):
         pg.mixer.init()
+        self.assetpath="Assets/Sounds/"
         self.deathsound_played=True
         self.Hitsound=[
-            pg.mixer.Sound("Assets/Sounds/Hurt1.mp3"),
-            pg.mixer.Sound("Assets/Sounds/Hurt2.mp3"),
-            pg.mixer.Sound("Assets/Sounds/Hurt3.mp3")
+            pg.mixer.Sound(self.assetpath+"Hit/HIT01.WAV"),
+            pg.mixer.Sound(self.assetpath+"Hit/HIT02.WAV"),
+            pg.mixer.Sound(self.assetpath+"Hit/HIT03.WAV"),
+            pg.mixer.Sound(self.assetpath+"Hit/HIT04.WAV"),
+            pg.mixer.Sound(self.assetpath+"Hit/HIT05.WAV"),
+            pg.mixer.Sound(self.assetpath+"Hit/HIT06.WAV")
         ]
         self.Deathsound=[
-            pg.mixer.Sound("Assets/Sounds/Hurt4.mp3"),
+            pg.mixer.Sound(self.assetpath+"Hurt/Hurt1.mp3"),
+            pg.mixer.Sound(self.assetpath+"Hurt/Hurt2.mp3"),
+            pg.mixer.Sound(self.assetpath+"Hurt/Hurt3.mp3"),
+            pg.mixer.Sound(self.assetpath+"Hurt/Hurt4.mp3")
         ]
     def PlaySound(self,soundarray):
         sound=random.choice(soundarray)
